52b00c2535133d9bbec9b9c22db3b50fa31797d5
alexanderchernetsky <alexvnderchernetsky@gmail.com>
2025-12-31 16:44:13 +0100

    Implement HolyGrail layout

--- src/pages/holy-grail/HolyGrail/HolyGrail.tsx
+++ src/pages/holy-grail/HolyGrail/HolyGrail.tsx
@@ -3,25 +3,44 @@
 // It consists of a header, footer, and three columns.
 // The left column contains navigation items, the middle column contains the page contents, and the right column contains ads.
 
+// Requirements:
+// Implement the Holy Grail layout using just CSS. You shouldn't need to change the HTML too much.
+//
+// Requirements
+// Header
+// Stretches horizontally across the whole page.
+// 60px tall.
+// Columns
+// Both the left and right columns have a fixed width of 100px.
+// The center column is fluid-width.
+// All the columns should have the same height, regardless of which column is the tallest.
+// Footer
+// Stretches horizontally across the whole page.
+// 100px tall.
+// The footer should be at the bottom of the page even if there is not enough content to fill up the viewport height.
 
+
+// Notes:
+// Use css grid to create 3 column layout: grid-cols-[100px_1fr_100px]
+// To implement "The footer should be at the bottom of the page even if there is not enough content to fill up the viewport height." use flex flex-col min-h-screen + flex-1
 const HolyGrail = () => {
     return (
-        <div>
-            <header>
-                Header
+        <div className="w-full flex flex-col min-h-screen">
+            <header className="bg-red-500 h-[60px] flex flex-col justify-center items-center p-4">
+                <h1 className="text-center">Holy Grail Layout</h1>
             </header>
-            <div>
-                <nav>
-                    Navigation
+            <div className="grid grid-cols-[100px_1fr_100px] flex-1">
+                <nav className="bg-blue-300 p-4">
+                    Navigation (left)
                 </nav>
-                <main>
+                <main className="bg-green-100 text-center p-4">
                     Main Content
                 </main>
-                <aside>
-                    Aside
+                <aside className="bg-yellow-200 p-4">
+                    Aside (right)
                 </aside>
             </div>
-            <footer>
+            <footer className="bg-purple-300 h-[100px] text-center p-4">
                 Footer
             </footer>
         </div>
--- src/pages/holy-grail/HolyGrailPage.tsx
+++ src/pages/holy-grail/HolyGrailPage.tsx
@@ -2,12 +2,7 @@ import HolyGrail from "./HolyGrail/HolyGrail";
 
 const HolyGrailPage = () => {
     return (
-        <main className="flex flex-col items-center justify-center w-full p-[32px]">
-            <h1 className="font-bold mb-8 text-2xl">Holy Grail Page</h1>
-            <section className="w-full max-w-5xl">
-                <HolyGrail />
-            </section>
-        </main>
+        <HolyGrail />
     )
 }
 
